e6c7441e6a50c8204400d37e40ce5ca7a5d93a5f
gcp <oilstand@shimesaba.jp>
2021-08-14 11:20:43 +0000

    うさ建夏祭りの文言修正

--- components/news/20210622001.vue
+++ components/news/20210622001.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="news_body_contents">
         <div class="news_description">
-            <p>AKUKIN建設は、Youtubeチャンネル「<a href="https://www.youtube.com/channel/UCp-5t9SrOQwXMU7iIjQfARg" target="_blank">Mio Channel 大神ミオ</a>」で行われた重役会議にて2021年6月27日に開催予定の「うさ建夏祭り」へ出店する事を発表いたしました。</p>
+            <p>AKUKIN建設は、Youtubeチャンネル「<a href="https://www.youtube.com/channel/UCp-5t9SrOQwXMU7iIjQfARg" target="_blank">Mio Channel 大神ミオ</a>」で行われた重役会議にて活動の再始動と、2021年6月27日に開催予定の「うさ建夏祭り」へ出店する事を発表いたしました。</p>
         </div>
         <div class="connect_videos">
             <h3>関連動画</h3>
             <h4>1.【AKUKIN建設】社長・・カムバック！重役会議【マイクラ】(2021/06/22)</h4>
             <p>Mio Channel 大神ミオ</p>
-            <YouTubeIFrame vid="kCHueXANn5k"/>
+            <YouTubeIFrame vid="kCHueXANn5k" />
+
+            <h4>2.【#うさ建夏祭り】「何が始まるんです？」「企画説明だ」【獅白ぼたん/ホロライブ】(2021/06/07)</h4>
+            <p>Botan Ch.獅白ぼたん</p>
+            <YouTubeIFrame vid="q_IXZIRCbwI" />
         </div>
     </div>
 </template>
